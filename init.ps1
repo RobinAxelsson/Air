23a663cf350aefc9649984836a4e03901a0773ef
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-03-03 20:57:07 +0100

    refac[dev]: init.ps1

--- init.ps1
+++ init.ps1
@@ -1,18 +1,33 @@
 #!/usr/bin/env pwsh
 
-function AddDirToPath($dir){
-    $fullDir = Join-Path $PSScriptRoot $dir
+$scriptsPath = Join-Path $PSScriptRoot scripts
+$endToEndIntegrationTests = Join-Path $PSScriptRoot test EndToEndIntegrationTests
+$airDomainFaresPath = Join-Path $PSScriptRoot src Air.Domain.Fares
+$airInterfaceCliPath = Join-Path $PSScriptRoot src Air.Interface.CLI
 
-    if (-not ($env:Path -split $pathSep -contains $fullDir)) {
-        $env:Path += [System.IO.Path]::PathSeparator + $fullDir
+EnsurePathExists $airDomainFaresPath
+EnsurePathExists $airInterfaceCliPath
+EnsurePathExists $scriptsPath
+EnsurePathExists $endToEndIntegrationTests
+
+# add script paths to run dev scripts
+AddDirToPath $scriptsPath
+AddDirToPath $endToEndIntegrationTests
+
+# add environment variables (some scripts depend on these)
+$env:_REPO_NAME_ = "Air"
+$env:_REPO_ROOT_ = $PSScriptRoot
+$env:_EF_PROJ_PATH_ = $airDomainFaresPath
+$env:_CLI_PROJ_PATH_ = $airInterfaceCliPath
+
+function AddDirToPath($dir){
+    if (-not ($env:Path -split [System.IO.Path]::PathSeparator -contains $dir)) {
+        $env:Path += [System.IO.Path]::PathSeparator + $dir
     }
 }
 
-AddDirToPath "scripts"
-AddDirToPath "test\EndToEndIntegrationTests"
-
-$env:_REPO_NAME_ = "Air"
-$env:_REPO_ROOT_ = $PSScriptRoot
-
-$env:_EF_PROJ_PATH_ = Join-Path $PSScriptRoot "src" "Air.Domain.Fares"
-$env:_CLI_PROJ_PATH_ = Join-Path $PSScriptRoot "src" "Air.Interface.CLI"
+function EnsurePathExists($dir){
+    if (-not (Test-Path $dir)) {
+        throw "Path does not exist: $dir"
+    }
+}
