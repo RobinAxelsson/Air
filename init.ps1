0a17cadaa2bccadb5144766e515e004cc45608d1
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-03-03 21:52:30 +0100

    fix: method definition order

--- init.ps1
+++ init.ps1
@@ -1,24 +1,26 @@
 #!/usr/bin/env pwsh
 
-$scriptsPath = Join-Path $PSScriptRoot scripts
-$endToEndIntegrationTests = Join-Path $PSScriptRoot test EndToEndIntegrationTests
-$airDomainFaresPath = Join-Path $PSScriptRoot src Air.Domain.Fares
-$airInterfaceCliPath = Join-Path $PSScriptRoot src Air.Interface.CLI
+function main(){
+    $scriptsPath = Join-Path $PSScriptRoot scripts
+    $endToEndIntegrationTests = Join-Path $PSScriptRoot test EndToEndIntegrationTests
+    $airDomainFaresPath = Join-Path $PSScriptRoot src Air.Domain.Fares
+    $airInterfaceCliPath = Join-Path $PSScriptRoot src Air.Interface.CLI
 
-EnsurePathExists $airDomainFaresPath
-EnsurePathExists $airInterfaceCliPath
-EnsurePathExists $scriptsPath
-EnsurePathExists $endToEndIntegrationTests
+    EnsurePathExists $airDomainFaresPath
+    EnsurePathExists $airInterfaceCliPath
+    EnsurePathExists $scriptsPath
+    EnsurePathExists $endToEndIntegrationTests
 
-# add script paths to run dev scripts
-AddDirToPath $scriptsPath
-AddDirToPath $endToEndIntegrationTests
+    # add script paths to run dev scripts
+    AddDirToPath $scriptsPath
+    AddDirToPath $endToEndIntegrationTests
 
-# add environment variables (some scripts depend on these)
-$env:_REPO_NAME_ = "Air"
-$env:_REPO_ROOT_ = $PSScriptRoot
-$env:_EF_PROJ_PATH_ = $airDomainFaresPath
-$env:_CLI_PROJ_PATH_ = $airInterfaceCliPath
+    # add environment variables (some scripts depend on these)
+    $env:_REPO_NAME_ = "Air"
+    $env:_REPO_ROOT_ = $PSScriptRoot
+    $env:_EF_PROJ_PATH_ = $airDomainFaresPath
+    $env:_CLI_PROJ_PATH_ = $airInterfaceCliPath
+}
 
 function AddDirToPath($dir){
     if (-not ($env:Path -split [System.IO.Path]::PathSeparator -contains $dir)) {
@@ -31,3 +33,5 @@ function EnsurePathExists($dir){
         throw "Path does not exist: $dir"
     }
 }
+
+main
