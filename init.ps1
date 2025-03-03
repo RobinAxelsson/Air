57734f22793e04b9e4a52e9d5c4dfc306b3456d1
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-03-04 00:22:32 +0100

    refac[dev]: init script

--- init.ps1
+++ init.ps1
@@ -1,37 +1,45 @@
 #!/usr/bin/env pwsh
 
-function main(){
-    $scriptsPath = Join-Path $PSScriptRoot scripts
-    $endToEndIntegrationTests = Join-Path $PSScriptRoot test EndToEndIntegrationTests
-    $airDomainFaresPath = Join-Path $PSScriptRoot src Air.Domain.Fares
-    $airInterfaceCliPath = Join-Path $PSScriptRoot src Air.Interface.CLI
+#nest these functions to avoid polluting the global scope
+function init_repository_environment(){
+    function main(){
+        $scriptsPath = Join-Path $PSScriptRoot scripts
+        $endToEndIntegrationTests = Join-Path $PSScriptRoot test EndToEndIntegrationTests
+        $airDomainFaresPath = Join-Path $PSScriptRoot src Air.Domain.Fares
+        $airInterfaceCliPath = Join-Path $PSScriptRoot src Air.Interface.CLI
+        $testResultsPath = Join-Path $PSScriptRoot temp test-results
 
-    EnsurePathExists $airDomainFaresPath
-    EnsurePathExists $airInterfaceCliPath
-    EnsurePathExists $scriptsPath
-    EnsurePathExists $endToEndIntegrationTests
+        EnsurePathExists $scriptsPath
+        EnsurePathExists $airDomainFaresPath
+        EnsurePathExists $airInterfaceCliPath
+        EnsurePathExists $endToEndIntegrationTests
+        EnsurePathExists $testResultsPath
 
-    # add script paths to run dev scripts
-    AddDirToPath $scriptsPath
-    AddDirToPath $endToEndIntegrationTests
+        # add script paths to run dev scripts
+        AddDirToEnvPath $scriptsPath
+        AddDirToEnvPath $endToEndIntegrationTests
 
-    # add environment variables (some scripts depend on these)
-    $env:_REPO_NAME_ = "Air"
-    $env:_REPO_ROOT_ = $PSScriptRoot
-    $env:_EF_PROJ_PATH_ = $airDomainFaresPath
-    $env:_CLI_PROJ_PATH_ = $airInterfaceCliPath
-}
-
-function AddDirToPath($dir){
-    if (-not ($env:Path -split [System.IO.Path]::PathSeparator -contains $dir)) {
-        $env:Path += [System.IO.Path]::PathSeparator + $dir
+        # add environment variables (some scripts depend on these)
+        $env:_REPO_NAME_ = "Air"
+        $env:_REPO_ROOT_ = $PSScriptRoot
+        $env:_EF_PROJ_PATH_ = $airDomainFaresPath
+        $env:_CLI_PROJ_PATH_ = $airInterfaceCliPath
+        $env:_TEST_RESULTS_PATH_ = $testResultsPath
     }
-}
 
-function EnsurePathExists($dir){
-    if (-not (Test-Path $dir)) {
-        throw "Path does not exist: $dir"
+    function AddDirToEnvPath($dir){
+        if (-not ($env:Path -split [System.IO.Path]::PathSeparator -contains $dir)) {
+            $env:Path += [System.IO.Path]::PathSeparator + $dir
+        }
     }
+
+    function EnsurePathExists($path){
+        if (-not (Test-Path $path)) {
+            throw "Path does not exist: $path"
+        }
+    }
+
+    main
 }
 
-main
+init_repository_environment
