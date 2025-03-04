f684660fd8a6b081bf98a2185ad1dc410007bc4c
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-03-04 21:19:30 +0100

    refac: rearrange

---
+++ tools/Air.Tools.Watch.Tests/ProcessTests.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+
+namespace Air.Tools.Watch;
+
+public class PowerShellTests
+{
+    [Fact]
+    public void Test_PowerShellScriptExecution()
+    {
+        string scriptPath = Path.Combine(Directory.GetCurrentDirectory(), "test.ps1");
+
+        //static void logAction(string mess) => Console.WriteLine(mess);
+
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "powershell.exe",
+            Arguments = $"-ExecutionPolicy Bypass -File \"{scriptPath}\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (Process process = new Process { StartInfo = psi })
+        {
+            //process.OutputDataReceived += (sender, e) => { if (e.Data != null) logAction(e.Data); };
+            //process.ErrorDataReceived += (sender, e) => { if (e.Data != null) logAction(e.Data); };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+        }
+    }
+}
