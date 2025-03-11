afb2dffb4214bbecb62bf7e0c7b11c03d479f0d6
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-03-11 17:59:44 +0100

    migrated change airport string

---
+++ src/Air.Domain.Fares/DataLayer/EF/Migrations/20250311165918_ChangeAirportToString.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Air.Domain.Fares.DataLayer.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAirportToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Origin",
+                table: "AirFlights",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Destination",
+                table: "AirFlights",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Origin",
+                table: "AirFlights",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Destination",
+                table: "AirFlights",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+    }
+}
