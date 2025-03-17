7e1836176e556c0cf560674d14079a23c1c9810b
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-03-17 11:11:43 +0100

    wip

---
+++ test/Air.Domain.Fares.Test.Class/ConfigurationProviderTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Air.Domain.Fares.Test.Class;
+
+public class ConfigurationProviderTests
+{
+    private static IConfigurationRoot CreateConfigurationRoot(string key, string value)
+    {
+        var appSettingsDictionary = new Dictionary<string, string?>()
+        {
+            { "AppSettings:" + key, value },
+        };
+
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddInMemoryCollection(appSettingsDictionary);
+        return configurationBuilder.Build();
+    }
+
+    private static ConfigurationProviderBase CreateConfigurationProvider(string key, string value)
+    {
+        var configurationRoot = CreateConfigurationRoot(key, value);
+        return new ConfigurationProvider(configurationRoot);
+    }
+
+    [Test]
+    [Category("Class Test")]
+    public async Task Run_setting()
+    {
+
+    }
+
+}
